2667bd4e5cb2d60f6a3339f73605b2882de5f6c2
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2020-10-16 11:58:35 +0900

    Initial commit

---
+++ Assets/Scripts/Controls/GameMainControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMainControl : MonoBehaviour
+{
+    // アドベンチャーテキスト
+    [SerializeField]private Text text;
+    private string Massagetext = "教室に人があふれる学校、教室に入り、僕は息をのみ挨拶をした。";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // メッセージエリアを初期化
+        text.text = "";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
